3941c52acce7bb957d727551fc7889a08fbffb4b
Stephen L Arnold <nerdboy@gentoo.org>
2021-02-08 13:07:32 -0800

    chg: ci: update artifact check job and fix wheel-check script
    Signed-off-by: Stephen L Arnold <nerdboy@gentoo.org>

--- .github/workflows/wheel-check.sh
+++ .github/workflows/wheel-check.sh
@@ -8,7 +8,7 @@ if ! [ -n $EXPECTED_WHEEL_COUNT ]; then
     exit 0
 fi
 
-WHEELS="./*.whl"
+WHEELS=$(find . -maxdepth 3 -name \*.whl)
 if [ $(echo $WHEELS | wc -w) -ne $EXPECTED_WHEEL_COUNT ]; then
     echo "Error: Expected $EXPECTED_WHEEL_COUNT wheels"
     exit 1
